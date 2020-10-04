23ef3beaaccc662f71b234c87396cdaf3bd49744
mariosexa13 <37478097+mariosexa13@users.noreply.github.com>
2020-10-04 13:22:33 +0300

    latest version

--- HackTues/Hack/Hack/views.py
+++ HackTues/Hack/Hack/views.py
@@ -1,7 +1,7 @@
 """
 Routes and views for the flask application.
 """
-
+import os
 from datetime import datetime
 from flask import render_template
 from Hack import app
@@ -35,3 +35,34 @@ def about():
         year=datetime.now().year,
         message='Your application description page.'
     )
+@app.route('/Run_Script')
+def run_script():
+    #coding: utf-8
+    print(os.getcwd())
+    tt = open("test2.txt", "r")
+    txt = tt.read()
+
+    z = txt.find(". .")
+    print("last", z)
+    word = "ковид"
+    a = txt.find(word)
+    print("word", a)
+    b = txt.find(".", 0, txt.find(word))
+    for b1 in range(0, z):
+        b1 = txt.find(".", b1, txt.find(word))
+        if b1>-1:
+            b = b1
+    print("start", b)
+    c = txt.find(". ", a+1, z+2)
+    print("end", c)
+
+    f = open("test.txt", "w")
+
+    for i in range(b+2, c+1):
+        f.write(txt[i])
+    f.close()
+    f = open("test.txt", "r")
+    print(f.read())
+
+
+
