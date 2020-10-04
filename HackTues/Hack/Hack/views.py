b55f8390ad546d754bebbe690a1f4c65bac287fe
mariosexa13 <37478097+mariosexa13@users.noreply.github.com>
2020-10-04 17:45:19 +0300

    17:45

--- HackTues/Hack/Hack/views.py
+++ HackTues/Hack/Hack/views.py
@@ -16,25 +16,66 @@ def home():
         year=datetime.now().year,
     )
 
-@app.route('/contact')
-def contact():
-    """Renders the contact page."""
+@app.route('/Features')
+def features():
+    """Renders the features page."""
     return render_template(
-        'contact.html',
-        title='Contact',
+        'features.html',
+        title='Features',
         year=datetime.now().year,
-        message='Your contact page.'
+        message='Your features page.'
     )
 
-@app.route('/about')
-def about():
-    """Renders the about page."""
+@app.route('/Index')
+def index():
+    """Renders the index page."""
     return render_template(
-        'about.html',
-        title='About',
+        'index.html',
+        title='Home Page',
         year=datetime.now().year,
-        message='Your application description page.'
+        message='Your home page.'
     )
+
+@app.route('/Worldwide')
+def pricing():
+    """Renders the worldwide page."""
+    return render_template(
+        'worldwide.html',
+        title='Pricing',
+        year=datetime.now().year,
+        message='Your pricing page.'
+    )
+
+@app.route('/Atwork')
+def atwork():
+    """Renders the atwork page."""
+    return render_template(
+        'atwork.html',
+        title='At work',
+        year=datetime.now().year,
+        message='Your at work page.'
+    )
+
+@app.route('/Shops')
+def shops():
+    """Renders the shops page."""
+    return render_template(
+        'shops.html',
+        title='Shops',
+        year=datetime.now().year,
+        message='Your shops page.'
+    )
+
+@app.route('/Inbg')
+def inbg():
+    """Renders the inbg page."""
+    return render_template(
+        'inbg.html',
+        title='In bg',
+        year=datetime.now().year,
+        message='Your in bg page.'
+    )
+
 @app.route('/Search')
 def run_script():
     #coding: utf-8
